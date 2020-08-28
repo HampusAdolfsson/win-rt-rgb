18cdb778d93d58cf59a63cf777c3918b4516450f
Hampus Adolfsson <hampus@adolfsson.email>
2020-08-28 03:24:27 +0200

    Remove webapp

--- webapp/src/app/models/outputspecification.ts
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-export interface IOutputSpecification {
-    ipAddress: string;
-    numberOfLeds: number;
-    saturationAdjustment: number;
-    blurRadius: number;
-    flipHorizontally: boolean;
-}
